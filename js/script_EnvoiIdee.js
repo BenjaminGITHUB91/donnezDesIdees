82df278ee0de6a9f5bf2233dd12bf3ce11436526
Lucas Echenique <lucas.echenique@outlook.fr>
2024-12-05 19:23:31 -0500

    Ajout du style et message de confirmation formulaire

--- js/script_EnvoiIdee.js
+++ js/script_EnvoiIdee.js
@@ -6,15 +6,38 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     })();
 
-    document.getElementById("envoiMail").addEventListener("submit", function(event) {
+    document.getElementById("Formulaire").addEventListener("submit", function(event) {
         event.preventDefault();
+        const nom = this.from_name.value;
+        const mail = this.from_mail.value;
+        const idee = this.message.value;
 
-        emailjs.sendForm("service_f4zjy6p", "template_e56mqi8", this)
+        if(!nom || !mail || !idee){
+            document.getElementById("messageErreur").style.display = "block";
+            document.getElementById("messageBravo").style.display = "none";
+            gsap.from('#messageErreur', {
+                y: -20,
+                opacity: 0,
+                duration: 0.5,
+                ease: "power2.out"
+            });
+        }else{
+            emailjs.sendForm("service_f4zjy6p", "template_e56mqi8", this)
             .then(() => {
                     console.log("Email bien envoyé");
+                    document.getElementById("messageErreur").style.display = "none";
+                    document.getElementById("messageBravo").style.display = "block";
+                    this.reset();
+                    gsap.from('#messageBravo', {
+                        y: -20,
+                        opacity: 0,
+                        duration: 0.5,
+                        ease: "power2.out"
+                    });
                 },(error) => {
                     console.log("Erreur : ", error);
                 }
             );
+        }
     })
 });
